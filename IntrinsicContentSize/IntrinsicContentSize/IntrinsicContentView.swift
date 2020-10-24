72feb661f2f8910980ad837138f41e732ed44c8a
DeveloperFenta <fenta0917@gmail.com>
2020-10-24 20:15:53 +0900

    Add IntrinsicContentSize Project

---
+++ IntrinsicContentSize/IntrinsicContentSize/IntrinsicContentView.swift
@@ -0,0 +1,17 @@
+//
+//  IntrinsicContentView.swift
+//  IntrinsicContentSize
+//
+//  Created by Fenta on 2020/10/24.
+//
+
+import UIKit
+
+@IBDesignable
+class IntrinsicContentView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 100, height: 50)
+    }
+
+}
